996aa63044de9e9319ef0212b8340cc86685a7a1
motifoye <yasquar@ya.ru>
2024-02-07 20:35:18 +0700

    write to file

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,51 @@
-use std::io;
 use std::io::Write;
-use std::fs;
 
-// fn su(a:i32,b:i32) -> i32 {
-//     a+b
-// }
+
+const FILE_PATH_1: &str = "C:\\Users\\User\\Desktop\\doc_1.txt";
+const FILE_PATH_2: &str = "C:\\Users\\User\\Desktop\\doc_2.txt";
+
 
 fn main() {
+    std::io::stdout().write_all(b"::4514851481::\n").unwrap();
+    println!("{}\n",sum(9,9));
     
-    io::stdout().write_all(b"::4514851481::\n").unwrap();
-    // println!("{:?}",su(9,9));
-    
-    
-    
-    
+    loop {
+        let buf = read_line();
+        
+        // if buf.as_bytes().eq(&[113,13,10]) 
+        // if buf.trim().as_bytes().eq(&[113]) 
+        if buf.trim().eq("q") {
+            println!("[dbg] input equal 'q'");
+            break;
+        }
+
+        write_to_file(FILE_PATH_1, buf);
+        
+    }
 }
 
+fn sum(a:i32,b:i32) -> i32 {  
+    a+b
+}
+
+fn write_to_file(path: &str, buf: String) {
+    use std::fs::File;
+    let mut file = File::options().append(true).open(path).expect("[ERR] open file");
+    match write!(file, "{}", buf) {
+        Ok(_) => println!("[OK] write file "),
+        Err(e) => println!("[ERR] write file | {:?}",e)
+    }
+}
+
+fn read_line() -> String {
+    use std::io::stdin;
+    let mut buf: String = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf
+}
 
 fn create_vec() {
-    let mut count = String::new();
-    
-    match io::stdin().read_line(&mut count) {
-        Ok(n) => {
-            println!("{n} bytes read");
-            // println!("{count}");
-        }
-        Err(error) => println!("error: {error}"),
-    }
-
-    let count: usize = count.trim().parse().expect("err: parse input");
+    let count: usize = read_line().trim().parse().expect("[ERR] fn: create_vec, can't parse input");
     
     let mut von = vec![0;count];
     for i in 0..count{
