18509cb6fc3a300c690049e171e4484f75c5dc69
motifoye <yasquar@ya.ru>
2024-02-28 02:36:13 +0700

    new path

--- src/config.rs
+++ src/config.rs
@@ -5,6 +5,7 @@ use std::fs::File;
 pub const PATH_DIR_DATA: &str = "data";
 pub const PATH_FILE_1: &str = "data/doc_1.txt";
 pub const PATH_FILE_2: &str = "data/doc_2.txt";
+pub const PATH_FILE_3: &str = "data/doc_3.txt";
 
 pub fn init(){
     let a = Path::new(PATH_DIR_DATA);
@@ -16,6 +17,10 @@ pub fn init(){
         File::create(a).unwrap();
     }
     let a = Path::new(PATH_FILE_2);
+    if !a.exists() {
+        File::create(a).unwrap();
+    }
+    let a = Path::new(PATH_FILE_3);
     if !a.exists() {
         File::create(a).unwrap();
     }
