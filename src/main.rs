dfa7f0aad6fdcd41e9ee04419b2d1af3d00369c7
motifoye <yasquar@ya.ru>
2024-02-28 10:07:55 +0700

    Create menu

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
+use std::arch::x86_64::_mm_pause;
+use crate::group_m::*;
+use crate::io_worker::{clear, pause, read_line};
+
 mod group_m;
 mod config;
 mod io_worker;
 
-
-
 fn main() {
     config::init();
-    group_m::task_m4();
+
+    loop {
+        clear();
+        menu();
+        match read_line().trim() {
+            "0" => {break},
+            "1" => task_m1(),
+            "2" => task_m2(),
+            "3" => task_m3(),
+            "4" => task_m4(),
+            "5" => task_m5(),
+            _ => {println!("Недопустимый вариант")}
+        }
+        pause();
+    }
+}
+
+fn menu() {
+    println!("1: Запись строк в файл циклом");
+    println!("2: Сумма чисел из файла");
+    println!("3: Копировать содерзимое из одного файла в другой");
+    println!("4: Поиск длинного слова в файле");
+    println!("5: Считает в файле строки, слова, символы");
 }
