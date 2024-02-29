8c421f1f2e1be3419fc9a06114540cad5f02cc52
motifoye <yasquar@ya.ru>
2024-02-29 14:46:45 +0700

    task_n2 complete

--- src/group_n.rs
+++ src/group_n.rs
@@ -1,3 +1,4 @@
+use rand::{random, Rng};
 use crate::io_worker::*;
 
 /// запрашивает у пользователя количество элементов в массиве,
@@ -22,4 +23,39 @@ pub fn task_n1() {
     }
 
     println!("{:?}",von);
+}
+
+/// запрашивает у пользователя размерность матрицы и
+/// выделяет динамическую память под эту матрицу.
+/// Заполните матрицу случайными числами и
+/// выведите ее на экран.
+pub fn task_n2() {
+    println!("::: TASK N2 :::");
+    println!("размер матрицы");
+
+    print("x: ");
+    let x = read_line().trim().parse::<usize>();
+    if x.is_err() {
+        println!("[E] {:?}",x.unwrap_err());
+        return;
+    }
+    let x = x.unwrap();
+
+    print("y: ");
+    let y = read_line().trim().parse::<usize>();
+    if y.is_err() {
+        println!("[E] {:?}",y.unwrap_err());
+        return;
+    }
+    let y = y.unwrap();
+
+    let mut matrix: Vec<Vec<i32>> = vec![vec![0;x];y];
+    for i in 0..y {
+        for j in 0..x {
+            matrix[i][j] = rand::thread_rng().gen_range(1..=100);
+            print(format!("{} ", matrix[i][j]).as_str());
+        }
+        println!();
+    }
+    println!("{matrix:?}")
 }
